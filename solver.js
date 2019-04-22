7643f359a42e70f090d3891be29dceaebc9b3676
JulGvoz <julgvoz@gmail.com>
2019-04-22 20:01:56 +0300

    Implemented checkIfSolved()

--- solver.js
+++ solver.js
@@ -1,23 +1,39 @@
+function arraysEqual(a, b) {
+  if (a === b) return true;
+  if (a == null || b == null) return false;
+  if (a.length != b.length) return false;
 
+  // If you don't care about the order of the elements inside
+  // the array, you should sort both arrays here.
+  // Please note that calling sort on an array will modify that array.
+  // you might want to clone your array first.
+
+  for (var i = 0; i < a.length; ++i) {
+    if (a[i] !== b[i]) return false;
+  }
+  return true;
+}
 
 function extractHints(ngActual) {
 	let hints = {};
 	hints.vertical = ngActual.verticalHints;
-	hints.horizontal = ng.horizontalHints;
+	hints.horizontal = ngActual.horizontalHints;
 	return hints;
 }
 
 function checkIfSolved(ng, hints) {
 	for (let i = 0; i < ng.height; i++) {
 		let ngHints = ng.getHints(i, true); // coord, horiz
-		if (ngHints != hints.horizontal[i]) {
+		//console.log(ngHints, hints.horizontal[i]);
+		if (!arraysEqual(ngHints, hints.horizontal[i])) {
 			return false;
 		}
 	}
 	
 	for (let i = 0; i < ng.width; i++) {
 		let ngHints = ng.getHints(i, false); // coord, horiz
-		if (ngHints != hints.vertical[i]) {
+		if (!arraysEqual(ngHints, hints.vertical[i])) {
+			//console.log(ngHints, hints.vertical[i]);
 			return false;
 		}
 	}
@@ -25,4 +41,9 @@ function checkIfSolved(ng, hints) {
 }
 
 function solveNonogram(ng, hints, badSolutions=[]) {
+	if (checkIfSolved(ng, hints)) {
+		return ng;
+	}
+	
+	
 }
